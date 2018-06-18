bb5dbf0ee3c94f0424981dcbdd5061a526332455
tiffanyyoon <yyoon032@rsdmo.org>
2018-06-18 21:06:02 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,6 +1,7 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-def greeting
-  puts local_greeting "Hello World"
+def local_greeting
+  puts "Hello World"
 end
+
 local_greeting
